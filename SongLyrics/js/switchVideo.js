132f67e806223b943f250064b07bd6644d968404
RichardSchool <43523766+RichardSchool@users.noreply.github.com>
2018-11-29 09:25:55 -0500

    Update switchVideo.js

--- SongLyrics/js/switchVideo.js
+++ SongLyrics/js/switchVideo.js
@@ -1,6 +1,6 @@
 var index = 0;
 function switchVideo(){
-let element = document.getElementByID("embed");
+let element = document.getElementById("embed");
 let code = [
 `<iframe width="806" height="453" float="middle" src="https://www.youtube.com/embed/dgha9S39Y6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Your browser needs to support inline frames inorder of this video to load.</iframe>`
 `<video width="806" height="453" controls><source src="assets/video.mp4" type="video/mp4">Your browser does not support the video tag.</video>`

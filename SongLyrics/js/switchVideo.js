f241414c477687a1ad53308da8e32be7c3e306c5
RichardSchool <43523766+RichardSchool@users.noreply.github.com>
2018-11-29 09:23:08 -0500

    Update switchVideo.js

--- SongLyrics/js/switchVideo.js
+++ SongLyrics/js/switchVideo.js
@@ -2,11 +2,8 @@ var index = 0;
 function switchVideo(){
 let element = document.getElementByID("embed");
 let code = [
-"<iframe width="806" height="453" float="middle" src="https://www.youtube.com/embed/dgha9S39Y6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Your browser needs to support inline frames inorder of this video to load.</iframe>"
-"<video width="806" height="453" controls>
-  <source src="assets/video.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>"
+`<iframe width="806" height="453" float="middle" src="https://www.youtube.com/embed/dgha9S39Y6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Your browser needs to support inline frames inorder of this video to load.</iframe>`
+`<video width="806" height="453" controls><source src="assets/video.mp4" type="video/mp4">Your browser does not support the video tag.</video>`
 ];
 
 if(index == 0){

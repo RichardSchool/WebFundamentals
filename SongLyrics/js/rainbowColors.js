27aeed4c8f34cee40fd9b7afb797adbd4ca75756
RichardSchool <43523766+RichardSchool@users.noreply.github.com>
2018-12-11 09:39:41 -0500

    Update rainbowColors.js

--- SongLyrics/js/rainbowColors.js
+++ SongLyrics/js/rainbowColors.js
@@ -2,7 +2,7 @@ var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 var index = 0;
 function changeColor(){
 $('.rainbow').each(element=>{
-element.style.color = color[index];
+element.style.color = colors[index];
 index++;
 if(index>colors.length){
 index = 0;

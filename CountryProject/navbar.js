c6d05f4b9f168ea0b7f2911d6e61a9733344d346
RichardSchool <43523766+RichardSchool@users.noreply.github.com>
2019-01-11 08:54:36 -0500

    Update navbar.js

--- CountryProject/navbar.js
+++ CountryProject/navbar.js
@@ -1,8 +1,8 @@
 function hideNav(){
-$("#navigation").children().prop('disabled',true);
-$("#currentPage").prop('href','javascript:showNav();');
+$(".navigation").children().prop('disabled',true);
+$(".currentPage").prop('href','javascript:showNav();');
 }
 function showNav(){
-$("#navigation").children().prop('disabled',true);
-$("#currentPage").prop('href','javascript:showNav();');
+$(".navigation").children().prop('disabled',true);
+$(".currentPage").prop('href','javascript:showNav();');
 }
